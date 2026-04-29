1f7077cd463b982e2e9167696fc76228e1ef29a7
brahmiines <brahmiines26@gmail.com>
2026-04-29 14:15:22 +0200

    Last commit

--- script.js
+++ script.js
@@ -53,3 +53,77 @@ function createBoard() {
         board.appendChild(card);
     });
 }
+
+
+function flip(card) {
+
+    if (lock || card === first || card.classList.contains("flip")) return;
+
+    flipSound.currentTime = 0;
+    flipSound.play();
+
+    card.classList.add("flip");
+
+    if (!first) {
+        first = card;
+        return;
+    }
+
+    second = card;
+    lock = true;
+
+    if (first.dataset.symbol === second.dataset.symbol) {
+
+        matchSound.currentTime = 0;
+        matchSound.play();
+
+        matched++;
+        reset();
+
+        if (matched === getSymbols().length / 2) {
+            nextLevel();
+        }
+
+    } else {
+        setTimeout(() => {
+            first.classList.remove("flip");
+            second.classList.remove("flip");
+            reset();
+        }, 800);
+    }
+}
+
+function reset() {
+    first = null;
+    second = null;
+    lock = false;
+}
+
+function nextLevel() {
+
+    if (currentLevel === 1) {
+
+        currentLevel = 2;
+        mission.textContent = "🐱 Aide Barbie à retrouver son chat !";
+
+        setTimeout(createBoard, 1000);
+
+    } else {
+
+        document.body.className = "win-bg";
+
+        winSound.currentTime = 0;
+        winSound.play();
+
+        win.classList.remove("hidden");
+    }
+}
+
+document.getElementById("restart").onclick = () => {
+    currentLevel = 1;
+    mission.textContent = "👗 Aide Barbie à retrouver ses vêtements";
+    win.classList.add("hidden");
+    createBoard();
+};
+
+createBoard();
